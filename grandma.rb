5f6e39346ed90d1177cd21f5714986f4bc0d7631
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-11 16:06:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == ''  
+  elsif phrase == 'I LOVE YOU GRANDMA!'  
+    return ""
   end
 end  
